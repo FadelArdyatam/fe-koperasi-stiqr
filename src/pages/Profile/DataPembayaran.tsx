cd23689c35e89a06ba2177478a44924592248dac
Aditya Novadianto <aditya.novadianto@gmail.com>
2024-12-01 21:12:45 +0700

    fix bug in DataPembayaran

--- src/pages/Profile/DataPembayaran.tsx
+++ src/pages/Profile/DataPembayaran.tsx
@@ -76,14 +76,14 @@ const DataPembayaran = () => {
     return (
         <div className="w-full flex flex-col min-h-screen items-center">
             <div className='w-full px-5 pt-5 pb-32 flex items-center justify-center bg-orange-400'>
-                {showContent.show === false || isAdding === false ? (
-                    <button onClick={buttonBack} className='absolute left-5 bg-transparent hover:bg-transparent'>
-                        <ChevronLeft className='scale-[1.3] text-white' />
-                    </button>
-                ) : (
+                {showContent.show === false ? (
                     <Link to={'/profile'} className='absolute left-5 bg-transparent hover:bg-transparent'>
                         <ChevronLeft className='scale-[1.3] text-white' />
                     </Link>
+                ) : (
+                    <button onClick={buttonBack} className='absolute left-5 bg-transparent hover:bg-transparent'>
+                        <ChevronLeft className='scale-[1.3] text-white' />
+                    </button>
                 )}
 
                 <p className='font-semibold m-auto text-xl text-white text-center'>{isAdding ? 'Tambah Data Pembayaran' : showEdit ? 'Edit Data Pembayaran' : 'Data Pembayaran'}</p>
