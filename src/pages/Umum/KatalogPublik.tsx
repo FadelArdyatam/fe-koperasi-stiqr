27031b51a370e02397640683114ab484f60d99f0
g.rald <hafizh.geraldy7@gmail.com>
2025-10-02 16:26:00 +0700

    feat: tampilan untuk non anggota koperasi

---
+++ src/pages/Umum/KatalogPublik.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const KatalogPublik = () => {
+  return (
+    <div>
+      <h1>Halaman Katalog Publik</h1>
+      <p>Placeholder untuk katalog publik (non-anggota).</p>
+    </div>
+  );
+};
+
+export default KatalogPublik;
